4b1ec5747b89ed56f1d44e54ceb7c2e7dad28e40
MahdiAkhbar <mahdi.akhbar32@gmail.com>
2024-05-28 15:38:07 +0330

    [14030308]: added time and date to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   constructor(
     private authService: AuthService
   ) { }
+
+  date!: string;
+  time!: string;
+
+  ngOnInit(): void {
+    interval(1000).subscribe(() => {
+      this.date = new Date().toLocaleDateString('fa-IR');
+      this.time = new Date().toLocaleTimeString('fa-IR');
+    })
+  }
 
   onLogout() {
     this.authService.logout();
